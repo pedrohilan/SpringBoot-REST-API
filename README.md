dd128a44d51eba4d9f82202d171defa5d24ef4ac
pedrohilan <pedro.hilan99@gmail.com>
2023-01-07 22:28:25 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# SpringBoot-REST-API
+
+END-POINTS: </br>
+
+/listartodos - GET</br>
+/salvar - POST</br>
+/deletar - DELETE</br>
+/buscaruserid - GET</br>
+/atualizar - PUT</br>
+/buscarnome - GET<br>
---
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -0,0 +1,91 @@
+package br.com.springboot.curso_jdev_treinamento.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.curso_jdev_treinamento.model.Usuario;
+import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
+
+/**
+ *
+ * A sample greetings controller to return greeting text
+ */
+@RestController
+public class GreetingsController {
+    
+	@Autowired /* IC/CD/ ou CDI Injeção de dependência */
+	private UsuarioRepository usuarioRepository;
+	
+	/**
+     *
+     * @param name the name to greet
+     * @return greeting text
+     */    
+    @GetMapping(value = "listartodos")
+    @ResponseBody /* Retorna os dados para o corpo da reposta */
+    public ResponseEntity<List<Usuario>> listarUsuarios(){
+    	List<Usuario> usuarios = usuarioRepository.findAll();
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+    }
+    
+    @PostMapping(value = "salvar")
+    @ResponseBody
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario /*Recebe os dados para salvar*/) {
+    	
+    	Usuario user = usuarioRepository.save(usuario);
+    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "atualizar")
+    @ResponseBody
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario /*Recebe os dados para salvar*/) {
+    	
+    	if(usuario.getId() == 0) {
+    		return new ResponseEntity<String>("Id não foi informado!!", HttpStatus.OK);
+    	}
+    	
+    	Usuario user = usuarioRepository.saveAndFlush(usuario);
+    	return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+    }
+    
+    @DeleteMapping(value = "deletar")
+    @ResponseBody
+    public ResponseEntity<String> deletar(@RequestParam Long iduser) {
+    	
+    	usuarioRepository.deleteById(iduser);
+    	return new ResponseEntity<String>("Usuário deletado com sucesso", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "buscaruserid")
+    @ResponseBody
+    public ResponseEntity<Usuario> buscaruserid(@RequestParam(name="iduser") Long iduser) {
+    	
+    	Usuario usuario = usuarioRepository.findById(iduser).get();
+    	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "buscarnome")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> buscarnome(@RequestParam(name="name") String name) {
+    	
+    	List<Usuario> usuario = usuarioRepository.buscarPorNome(name.trim().toUpperCase());
+    	
+    	return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);
+    }
+}
